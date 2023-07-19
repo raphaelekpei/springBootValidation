ce25362ebcb4a8faf6f75b08555d8bc4580719a3
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-19 20:52:27 +0100

    Commit 11006

---
+++ src/main/java/com/raphael/springbootvalidation/dto/response/CreateUserResponse.java
@@ -0,0 +1,10 @@
+package com.raphael.springbootvalidation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateUserResponse {
+    private String message;
+}
