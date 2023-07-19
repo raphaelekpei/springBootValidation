f72cedd1f41479774fe84115101b65c07b51b887
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-19 20:52:45 +0100

    Commit 11007

---
+++ src/main/java/com/raphael/springbootvalidation/constants/Gender.java
@@ -0,0 +1,31 @@
+package com.raphael.springbootvalidation.constants;
+
+public enum Gender {
+
+    MALE,
+    FEMALE,
+    NON_BINARY,
+    GENDERQUEER,
+    GENDERFLUID,
+    AGENDER,
+    BIGENDER,
+    TWO_SPIRIT,
+    ANDROGYNOUS,
+    DEMIGENDER,
+    NEUTROIS,
+    GENDER_QUESTIONING,
+    GENDER_NONCONFORMING,
+    TRANSGENDER,
+    TRANSMASCULINE,
+    TRANSFEMININE,
+    CISGENDER,
+    INTERSEX,
+    PANGENDER,
+    GENDER_VARIANT,
+    GENDER_EXPANSIVE,
+    GENDER_CREATIVE,
+    THIRD_GENDER,
+    OTHER,
+    PREFER_NOT_TO_SAY
+
+}
